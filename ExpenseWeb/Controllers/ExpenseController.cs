153d947cbb178c764eb47f4c85862f1180b977f2
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-11 13:07:52 +0200

    Add mock database.

---
+++ ExpenseWeb/Controllers/ExpenseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseWeb.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseWeb.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly IExpenseDatabase _expenseDatabase;
+
+        public ExpenseController(IExpenseDatabase expenseDatabase)
+        {
+            _expenseDatabase = expenseDatabase;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
