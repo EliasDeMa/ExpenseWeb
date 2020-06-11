b1c2fab4bafb6a0351c2beab727fda2b634703d0
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-11 14:12:24 +0200

    Add minimumvalue to amount.

--- ExpenseWeb/Models/ExpenseCreateViewModel.cs
+++ ExpenseWeb/Models/ExpenseCreateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace ExpenseWeb.Models
     {
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Amount has to be positive.")]
         public decimal Amount { get; set; }
     }
 }
