5dbc8c0d56ce17ae4e042bcddf6dc0ef7986985e
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-11 16:09:01 +0200

    Start of statistics page.

---
+++ ExpenseWeb/Controllers/StatisticsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using ExpenseWeb.Database;
+using ExpenseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseWeb.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly IExpenseDatabase _expenseDatabase;
+
+        public StatisticsController(IExpenseDatabase expenseDatabase)
+        {
+            _expenseDatabase = expenseDatabase;
+        }
+
+        public IActionResult Index()
+        {
+            decimal highest = _expenseDatabase
+                .GetExpenses()
+                .Select(x => x.Amount)
+                .Max();
+
+            var statistics = new StatisticsIndexViewModel
+            {
+                Highest = highest,
+            };
+
+            return View(statistics);
+        }
+    }
+}
---
+++ ExpenseWeb/Models/StatisticsIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseWeb.Models
+{
+    public class StatisticsIndexViewModel
+    {
+        public decimal Highest { get; set; }
+    }
+}
