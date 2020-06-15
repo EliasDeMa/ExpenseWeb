a8b28084dad64f7029bed597f01622f5a1c9647b
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-15 10:15:46 +0200

    Add photoService to save and delete photos

---
+++ ExpenseWeb/Services/IPhotoService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseWeb.Services
+{
+    public interface IPhotoService
+    {
+        void AddPhoto(string fileName, IFormFile file);
+        void DeletePhoto(string fileName);
+    }
+}
---
+++ ExpenseWeb/Services/PhotoService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseWeb.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public PhotoService(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public void AddPhoto(string fileName, IFormFile photo)
+        {
+            var path = Path.Combine(_hostEnvironment.WebRootPath, "expense-pics", fileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            photo.CopyTo(stream);
+        }
+
+        public void DeletePhoto(string fileName)
+        {
+            var prevPath = Path.Combine(_hostEnvironment.WebRootPath, "photos", fileName);
+            System.IO.File.Delete(prevPath);
+        }
+    }
+}
