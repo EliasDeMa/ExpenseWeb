72d76791b5cf4558c4a1d41db43c08913a60889a
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-15 14:30:42 +0200

    db context to communicate with db.

---
+++ ExpenseWeb/Database/ExpenseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseWeb.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseWeb.Database
+{
+    public class ExpenseDbContext : DbContext
+    {
+        public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
