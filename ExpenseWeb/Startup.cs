17bd42177aa4b099c1afc29812de69c30df53ea3
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-19 14:26:51 +0200

    different default page, since you have to be logged in for the functionality pages.

--- ExpenseWeb/Startup.cs
+++ ExpenseWeb/Startup.cs
@@ -65,7 +65,7 @@ namespace ExpenseWeb
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Expense}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
